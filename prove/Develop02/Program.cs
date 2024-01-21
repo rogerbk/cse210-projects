69c6e7733e58c3aeef9697814c56a5d8d7875f34
rogerbk <rogercr2002@hotmail.com>
2024-01-20 22:14:20 -0600

    Develop02

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,151 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+
+class JournalEntry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string EntryDate { get; set; }
+
+    public JournalEntry(string prompt, string response)
+    {
+        Prompt = prompt;
+        Response = response;
+        EntryDate = DateTime.Now.ToShortDateString();
+    }
+
+    public override string ToString()
+    {
+        return $"{EntryDate}|{Prompt}|{Response}";
+    }
+}
+
+class Journal
+{
+    private List<JournalEntry> entries;
+
+    public Journal()
+    {
+        entries = new List<JournalEntry>();
+    }
+
+    public void AddEntry(string prompt, string response)
+    {
+        entries.Add(new JournalEntry(prompt, response));
+    }
+
+    public void DisplayJournal()
+    {
+        foreach (var entry in entries)
+        {
+            Console.WriteLine(entry);
+        }
+    }
+
+    public void SaveJournal(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                outputFile.WriteLine(entry);
+            }
+        }
+    }
+
+    public void LoadJournal(string filename)
+    {
+        entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            string date = parts[0];
+            string prompt = parts[1];
+            string response = parts[2];
+            entries.Add(new JournalEntry(prompt, response) { EntryDate = date });
+        }
+    }
+}
+
+class JournalApp
+{
+    private Journal journal;
+    private string[] prompts = new string[]
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public JournalApp()
+    {
+        journal = new Journal();
+    }
+
+    private string GetRandomPrompt()
+    {
+        Random rnd = new Random();
+        int index = rnd.Next(prompts.Length);
+        return prompts[index];
+    }
+
+    public void Run()
+    {
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    string response = Console.ReadLine();
+                    journal.AddEntry(prompt, response);
+                    break;
+                case "2":
+                    journal.DisplayJournal();
+                    break;
+                case "3":
+                    Console.WriteLine("Enter a filename to save:");
+                    string saveFileName = Console.ReadLine();
+                    journal.SaveJournal(saveFileName);
+                    break;
+                case "4":
+                    Console.WriteLine("Enter a filename to load:");
+                    string loadFileName = Console.ReadLine();
+                    journal.LoadJournal(loadFileName);
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop02 World!");
+        JournalApp app = new JournalApp();
+        app.Run();
     }
 }
